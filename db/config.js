483b7ccc5f1461a85f3175b897b6e5384b8632c0
Priyanka Kumari <priyankakumari_ee21b9_01@dtu.ac.in>
2025-03-05 13:17:04 +0530

    Added all remainig files

---
+++ db/config.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost:27017/e-commerce');
+
+mongoose.connect('mongodb://127.0.0.1:27017/e-commerce');
+// mongoose.connect('mongodb://127.0.0.1:27017/e-commerce', { useNewUrlParser: true, useUnifiedTopology: true });
